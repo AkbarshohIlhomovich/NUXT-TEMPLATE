a9788da2280037e07c07d725b4c24e18f5c83990
AkbarshohIlhomovich <bloghack9@gmail.com>
2025-09-16 09:38:50 +0500

    O‘zgarishlar - 2025-09-16 09:38:50

---
+++ app/components/Welcome.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1 class="w-full h-screen flex items-center justify-center text-3xl">Hello world!</h1>
+</template>
--- app/pages/not-available.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="flex items-center justify-center min-h-screen">
-    <h1>Bu sayt faqat mobil qurilmalarda ishlaydi</h1>
-  </div>
-</template>
